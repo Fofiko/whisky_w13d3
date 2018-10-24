96205dce290336c3f74992580335eb511297d410
Fofiko <foteinid@gmail>
2018-10-24 17:25:14 +0100

    Passed get all the distilleries for a particular region

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryImpl.java
@@ -0,0 +1,32 @@
+package com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+public class DistilleryRepositoryImpl implements DistilleryRepositoryCustom {
+
+    @Autowired
+    EntityManager entityManager;
+
+
+    //TODO: get all the distilleries for a particular region
+    @Transactional
+    public List<Distillery> getAllDistilleriesFromRegion(String region){
+        List<Distillery> results = null;
+
+        Session session = entityManager.unwrap(Session.class);
+
+        Criteria cr = session.createCriteria(Distillery.class);
+        cr.add(Restrictions.eq("region", region));
+        results = cr.list();
+        return results;
+    }
+
+}
---
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -0,0 +1,30 @@
+package com.codeclan.example.WhiskyTracker;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository.DistilleryRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhiskyTrackerApplicationTests {
+
+	@Autowired
+	DistilleryRepository distilleryRepository;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void canGetAllDistilleriesFromRegion(){
+		List<Distillery> results = distilleryRepository.getAllDistilleriesFromRegion("Highland");
+
+	}
+
+}
